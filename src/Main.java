5b457474d896acd278c238f61024534adf0428ce
Lê Ngọc <minngoc.1213@gmail.com>
2023-06-16 17:13:33 +0700

    Update the function to allow for re-entering data after inputting incorrectly.

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,15 @@
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         int inputQuantity;
-        ArrayList<Double> inputList = new ArrayList<>();
+        ArrayList<Integer> inputList = new ArrayList<>();
 
-        System.out.print("Enter the quantity number: ");
-        try {
-            inputQuantity = scanner.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("Input error!");
-            return;
-        }
-
-        if (inputQuantity < 2) {
-            System.out.println("Quantity must be greater than 1!");
-            return;
-        }
+        inputQuantity = readNumber("Enter the quantity number: ");
 
         for (int i = 0; i < inputQuantity; i++) {
-            try {
-                inputList.add(scanner.nextDouble());
-            } catch (InputMismatchException e) {
-                System.out.println("Input error!");
-                return;
-            }
+            inputList.add(readNumber(""));
         }
 
         double largest = inputList.get(0);
@@ -54,7 +35,32 @@ public class Main {
             System.out.println("The 2nd largest number is " + secondLargest);
         }
 
-        scanner.close();
+    }
+
+    public static int readNumber(String intro) {
+        int inputNumber;
+        System.out.print(intro);
+
+        do {
+            inputNumber = handleException();
+            if (inputNumber < 1) {
+                System.out.print("Please enter an integer number greater than 0: ");
+            }
+        } while (inputNumber < 1);
+
+        return inputNumber;
+    }
+
+    public static int handleException() {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber;
+
+        try {
+            inputNumber = scanner.nextInt();
+        } catch (Exception e) {
+            return 0;
+        }
+        return inputNumber;
     }
 
 }
